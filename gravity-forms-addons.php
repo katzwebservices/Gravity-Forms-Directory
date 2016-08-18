3260dae28f71ddc3a13e5ab599361121732d036d
Zack Katz <zack@katzwebservices.com>
2016-08-17 22:27:24 -0600

    Version bump

--- gravity-forms-addons.php
+++ gravity-forms-addons.php
@@ -4,7 +4,7 @@ Plugin Name: 	Gravity Forms Directory & Addons
 Plugin URI: 	http://katz.co/gravity-forms-addons/
 Description: 	Turn <a href="http://katz.si/gravityforms">Gravity Forms</a> into a great WordPress directory...and more!
 Author: 		Katz Web Services, Inc.
-Version: 		3.8
+Version: 		3.8.1
 Author URI:		https://gravityview.co
 Text Domain:    gravity-forms-addons
 License:		GPLv2 or later
@@ -36,7 +36,7 @@ class GFDirectory {
 
 	private static $path = "gravity-forms-addons/gravity-forms-addons.php";
 	private static $slug = "gravity-forms-addons";
-	private static $version = "3.8";
+	private static $version = "3.8.1";
 	private static $min_gravityforms_version = "1.5";
 
 	public static function directory_defaults( $args = array() ) {
