41531e976b9cf13aa2f901d8dd70b9173cd7c0f2
Shamali Sulakhe <shamali.sulakhe@wisdmlabs.com>
2020-11-11 23:44:08 +0530

    Move entry detail content to view file

--- includes/class-gf-directory.php
+++ includes/class-gf-directory.php
@@ -14,11 +14,6 @@
 
 class GFDirectory {
 
-	// private static $path = "gravity-forms-addons/gravity-forms-addons.php";
-	// private static $slug = "gravity-forms-addons";
-	// private static $version = "4.2";
-	// private static $min_gravityforms_version = "2.2.3.12";
-
 	/**
 	 * Instance of this class.
 	 *
@@ -37,6 +32,8 @@ class GFDirectory {
 	 */
 	public function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+		add_action( 'plugins_loaded', array( $this, 'register_text_domain' ), 9 );
+		add_action( 'init', array( 'GFDirectory', 'init' ) );
 		$this->init_classes();
 	}
 
@@ -53,19 +50,6 @@ class GFDirectory {
 		include_once( GF_DIRECTORY_PATH . 'includes/class-gf-directory-edit-form.php' );
 		include_once( GF_DIRECTORY_PATH . 'includes/admin/class-gf-directory-admin.php' );
 		include_once( GF_DIRECTORY_PATH . 'includes/class-gravity-forms-lead-creator.php' );
-	}
-
-	/**
-	 * Hook into actions and filters.
-	 *
-	 * @since      1.0.0
-	 *
-	 * @return     void
-	 */
-	private function init_hooks() {
-		// add_action( 'plugins_loaded', array( $this, 'register_text_domain' ), 9 );
-		add_action( 'plugins_loaded', array( $this, 'register_text_domain' ), 9 );
-		add_action( 'init', array( 'GFDirectory', 'init' ) );
 	}
 
 	/**
@@ -110,6 +94,14 @@ class GFDirectory {
 		load_plugin_textdomain( 'gravity-forms-addons', false, GF_DIRECTORY_PATH . '/languages' );
 	}
 
+	/**
+	 * Get array of default directory arguments.
+	 *
+	 * @since     TODO
+	 *
+	 * @param  array  $args    Array of args.
+	 * @return array  $options Parsed array with default keys.
+	 */
 	public static function directory_defaults( $args = array() ) {
 		$defaults = array(
 			'form'          => 1,
@@ -238,6 +230,12 @@ class GFDirectory {
 		return apply_filters( 'kws_gf_directory_defaults', $options );
 	}
 
+	/**
+	 * Include files and load functionality on loading.
+	 *
+	 * @since     TODO
+	 *
+	 */
 	public function plugins_loaded() {
 
 		// Include files.
@@ -246,11 +244,14 @@ class GFDirectory {
 		if ( self::is_gravity_page() ) {
 			self::load_functionality();
 		}
-
-		$this->init_hooks();
 	}
 
-	//Plugin starting point. Will load appropriate files
+	/**
+	 * Plugin starting point. Will load appropriate files.
+	 *
+	 * @since     TODO
+	 *
+	 */
 	public static function init() {
 		global $current_user;
 
@@ -302,24 +303,41 @@ class GFDirectory {
 
 	}
 
-	//Target of Member plugin filter. Provides the plugin with Gravity Forms lists of capabilities
+	/**
+	 * Target of Member plugin filter. Provides the plugin with Gravity Forms lists of capabilities.
+	 *
+	 * @since     TODO
+	 *
+	 */
 	public static function members_get_capabilities( $caps ) {
 		return array_merge( $caps, array( "gravityforms_directory", "gravityforms_directory_uninstall" ) );
 	}
 
+	/**
+	 * Check if Gravity Forms plugin is installed.
+	 *
+	 * @since     TODO
+	 *
+	 * @return bool True if Gravity Forms is installed, false otherwise.
+	 *
+	 */
 	public static function is_gravityforms_installed() {
 		return class_exists( "RGForms" );
 	}
 
+	/**
+	 * Load functionality.
+	 *
+	 * @since     TODO
+	 *
+	 */
 	static private function load_functionality() {
-
-		//register_deactivation_hook( __FILE__, array( 'GFDirectory', 'uninstall' ) );
 
 		$settings = self::get_settings();
 		extract( $settings );
 
 		if ( $referrer ) {
-			// Load Joost's referrer tracker
+			// Load Joost's referrer tracker.
 			@include_once( GF_DIRECTORY_PATH. '/gravity-forms-referrer.php' );
 		}
 
@@ -441,12 +459,12 @@ class GFDirectory {
 		$settings = array();
 		if ( ! empty( $colorboxSettings ) && is_array( $colorboxSettings ) ) {
 			foreach ( $colorboxSettings as $key => $value ) {
-				if ( $value === NULL ) {
+				if ( NULL === $value ) {
 					continue;
 				}
-				if ( $value === true ) {
+				if ( true === $value ) {
 					$value = 'true';
-				} elseif ( empty( $value ) && $value !== 0 ) {
+				} elseif ( empty( $value ) && 0 !== $value ) {
 					$value = 'false';
 				} else {
 					$value = '"' . $value . '"';
@@ -744,242 +762,7 @@ class GFDirectory {
 
 		extract( $options );
 
-		?>
-		<table cellspacing="0" class="widefat fixed entry-detail-view">
-			<?php
-			$title = str_replace( '%%formtitle%%', $Form["title"], str_replace( '%%leadid%%', $lead['id'], $entrydetailtitle ) );
-			if ( ! empty( $title ) && $inline ) { ?>
-				<thead>
-				<tr>
-					<th id="details" colspan="2" scope="col">
-						<?php
-						$title = apply_filters( 'kws_gf_directory_detail_title', apply_filters( 'kws_gf_directory_detail_title_' . (int) $lead['id'], array(
-							$title,
-							$lead,
-						), true ), true );
-						if ( is_array( $title ) ) {
-							echo $title[0];
-						} else {
-							echo $title;
-						}
-						?>
-					</th>
-				</tr>
-				</thead>
-				<?php
-			}
-			?>
-			<tbody>
-			<?php
-			$count              = 0;
-			$has_product_fields = false;
-			$field_count        = sizeof( $Form["fields"] );
-			$display_value      = '';
-			foreach ( $Form["fields"] as $field ) {
-
-				// Don't show fields defined as hide in single.
-				if ( ! empty( $field->hideInSingle ) ) {
-					if ( self::has_access( "gravityforms_directory" ) ) {
-						echo "\n\t\t\t\t\t\t\t\t\t" . '<!-- ' . sprintf( esc_html__( '(Admin-only notice) Field #%d not shown: "Hide This Field in Single Entry View" was selected.', 'gravity-forms-addons' ), $field->id ) . ' -->' . "\n\n";
-					}
-					continue;
-				}
-
-				$count ++;
-				$is_last = $count >= $field_count ? true : false;
-
-				switch ( RGFormsModel::get_input_type( $field ) ) {
-					case "section" :
-						if ( ! GFCommon::is_section_empty( $field, $Form, $lead ) || $display_empty_fields ) {
-							$count ++;
-							$is_last = $count >= $field_count ? true : false;
-							?>
-							<tr>
-								<td colspan="2"
-								    class="entry-view-section-break<?php echo $is_last ? " lastrow" : "" ?>"><?php echo esc_html( GFCommon::get_label( $field ) ) ?></td>
-							</tr>
-							<?php
-						}
-						break;
-
-					case "captcha":
-					case "html":
-					case "password":
-					case "page":
-						//ignore captcha, html, password, page field
-						break;
-
-					case "post_image" :
-						$value      = RGFormsModel::get_lead_field_value( $lead, $field );
-						$valueArray = explode( "|:|", $value );
-
-						@list( $url, $title, $caption, $description ) = $valueArray;
-
-						if ( ! empty( $url ) ) {
-							$value = $display_value = self::render_image_link( $url, $lead, $options, $title, $caption, $description );
-						}
-						break;
-
-					default :
-						//ignore product fields as they will be grouped together at the end of the grid
-						if ( GFCommon::is_product_field( $field->type ) ) {
-							$has_product_fields = true;
-							continue;
-						}
-
-						$value         = RGFormsModel::get_lead_field_value( $lead, $field );
-						$display_value = GFCommon::get_lead_field_display( $field, $value, $lead["currency"] );
-						break;
-
-				} // end switch
-
-				$display_value = apply_filters( "gform_entry_field_value", $display_value, $field, $lead, $Form );
-				if ( $display_empty_fields || ! empty( $display_value ) || $display_value === "0" ) {
-					$count ++;
-					$is_last  = $count >= $field_count && ! $has_product_fields ? true : false;
-					$last_row = $is_last ? " lastrow" : "";
-
-					$display_value = empty( $display_value ) && $display_value !== "0" ? "&nbsp;" : $display_value;
-
-					$content = '
-                            <tr>
-                                <th colspan="2" class="entry-view-field-name">' . esc_html( GFCommon::get_label( $field ) ) . '</th>
-                            </tr>
-                            <tr>
-                                <td colspan="2" class="entry-view-field-value' . $last_row . '">' . $display_value . '</td>
-                            </tr>';
-
-					$content = apply_filters( "gform_field_content", $content, $field, $value, $lead["id"], $Form["id"] );
-
-					echo $content;
-
-				}
-
-			} // End foreach
-
-			$products = array();
-			if ( $has_product_fields ) {
-				$products = GFCommon::get_product_fields( $Form, $lead );
-				if ( ! empty( $products["products"] ) ) {
-					?>
-					<tr>
-						<td colspan="2"
-						    class="entry-view-field-name"><?php echo apply_filters( "gform_order_label_{$Form["id"]}", apply_filters( "gform_order_label", __( "Order", "gravityforms" ), $Form["id"] ), $Form["id"] ) ?></td>
-					</tr>
-					<tr>
-						<td colspan="2" class="entry-view-field-value lastrow">
-							<table class="entry-products" cellspacing="0" width="97%">
-								<colgroup>
-									<col class="entry-products-col1">
-									<col class="entry-products-col2">
-									<col class="entry-products-col3">
-									<col class="entry-products-col4">
-								</colgroup>
-								<thead>
-								<th scope="col"><?php echo apply_filters( "gform_product_{$Form['id']}", apply_filters( "gform_product", __( "Product", "gravityforms" ), $Form['id'] ), $Form['id'] ) ?></th>
-								<th scope="col"
-								    class="textcenter"><?php echo apply_filters( "gform_product_qty_{$Form['id']}", apply_filters( "gform_product_qty", __( "Qty", "gravityforms" ), $Form['id'] ), $Form['id'] ) ?></th>
-								<th scope="col"><?php echo apply_filters( "gform_product_unitprice_{$Form['id']}", apply_filters( "gform_product_unitprice", __( "Unit Price", "gravityforms" ), $Form['id'] ), $Form['id'] ) ?></th>
-								<th scope="col"><?php echo apply_filters( "gform_product_price_{$Form['id']}", apply_filters( "gform_product_price", __( "Price", "gravityforms" ), $Form['id'] ), $Form['id'] ) ?></th>
-								</thead>
-								<tbody>
-								<?php
-
-								$total = 0;
-								foreach ( $products["products"] as $product ) {
-									?>
-									<tr>
-										<td>
-											<div class="product_name"><?php echo esc_html( $product["name"] ) ?></div>
-											<ul class="product_options">
-												<?php
-												$price = GFCommon::to_number( $product["price"] );
-												if ( is_array( rgar( $product, "options" ) ) ) {
-													$count = sizeof( $product["options"] );
-													$index = 1;
-													foreach ( $product["options"] as $option ) {
-														$price += GFCommon::to_number( $option["price"] );
-														$class = $index == $count ? " class='lastitem'" : "";
-														$index ++;
-														?>
-														<li<?php echo $class ?>><?php echo $option["option_label"] ?></li>
-														<?php
-													}
-												}
-												$subtotal = floatval( $product["quantity"] ) * $price;
-												$total += $subtotal;
-												?>
-											</ul>
-										</td>
-										<td class="textcenter"><?php echo $product["quantity"] ?></td>
-										<td><?php echo GFCommon::to_money( $price, $lead["currency"] ) ?></td>
-										<td><?php echo GFCommon::to_money( $subtotal, $lead["currency"] ) ?></td>
-									</tr>
-									<?php
-								}
-								$total += floatval( $products["shipping"]["price"] );
-								?>
-								</tbody>
-								<tfoot>
-								<?php
-								if ( ! empty( $products["shipping"]["name"] ) ) {
-									?>
-									<tr>
-										<td colspan="2" rowspan="2" class="emptycell">&nbsp;</td>
-										<td class="textright shipping"><?php echo $products["shipping"]["name"] ?></td>
-										<td class="shipping_amount"><?php echo GFCommon::to_money( $products["shipping"]["price"], $lead["currency"] ) ?>
-											&nbsp;</td>
-									</tr>
-									<?php
-								}
-								?>
-								<tr>
-									<?php
-									if ( empty( $products["shipping"]["name"] ) ) {
-										?>
-										<td colspan="2" class="emptycell">&nbsp;</td>
-										<?php
-									}
-									?>
-									<td class="textright grandtotal"><?php esc_html_e( "Total", "gravityforms" ) ?></td>
-									<td class="grandtotal_amount"><?php echo GFCommon::to_money( $total, $lead["currency"] ) ?></td>
-								</tr>
-								</tfoot>
-							</table>
-						</td>
-					</tr>
-
-					<?php
-				}
-			}
-
-			// Edit link
-			if (
-				! empty( $options['useredit'] ) && is_user_logged_in() && intval( $current_user->ID ) === intval( $lead['created_by'] ) || // Is user who created the entry
-				! empty( $options['adminedit'] ) && self::has_access( "gravityforms_directory" ) // Or is an administrator
-			) {
-
-				if ( ! empty( $options['adminedit'] ) && self::has_access( "gravityforms_directory" ) ) {
-					$editbuttontext = apply_filters( 'kws_gf_directory_edit_entry_text_admin', __( "Edit Entry", 'gravity-forms-addons' ) );
-				} else {
-					$editbuttontext = apply_filters( 'kws_gf_directory_edit_entry_text_user', __( "Edit Your Entry", 'gravity-forms-addons' ) );
-				}
-
-				?>
-				<tr>
-					<th scope="row"
-					    class="entry-view-field-name"><?php echo esc_html( apply_filters( 'kws_gf_directory_edit_entry_th', __( "Edit", "gravity-forms-addons" ) ) ); ?></th>
-					<td class="entry-view-field-value useredit"><a
-							href="<?php echo esc_url( add_query_arg( array( 'edit' => wp_create_nonce( 'edit' . $lead['id'] . $Form["id"] ) ) ) ); ?>"><?php echo $editbuttontext; ?></a>
-					</td>
-				</tr>
-				<?php
-			}
-
-			?>
-			</tbody>
-		</table>
-		<?php
+		include_once( GF_DIRECTORY_PATH . 'includes/views/html-entry-detail.php' );
 	}
 
 	static public function get_admin_only( $form, $adminOnly = array() ) {
